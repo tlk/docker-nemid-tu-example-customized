162c4aef3b82bfa052686f64ea9dae93adb3eb9a
Thomas L. Kjeldsen <tlk@closureconsulting.com>
2015-05-05 17:19:07 +0200

    build url from hostname on systems without boot2docker

--- print-url.sh
+++ print-url.sh
@@ -2,5 +2,7 @@
 
 BOOT2DOCKER=$(which boot2docker)
 if [ $? = "0" ]; then
-  echo open http://$($BOOT2DOCKER ip):8080
+  echo http://$($BOOT2DOCKER ip):8080
+else
+  echo http://$(hostname):8080
 fi
